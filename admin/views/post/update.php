5d76e0aabf19f6ec31c27d6171f945e9d3fc1d0c
ivanov.a.a.23@mail.ru <rainboowbee@gmail.com>
2024-04-19 15:19:53 +0300

    Русифицировал страницу изменения поста

--- admin/views/post/update.php
+++ admin/views/post/update.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var common\models\Post $model */
 
-$this->title = 'Update Post: ' . $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Posts', 'url' => ['index']];
+$this->title = 'Изменение поста: ' . $model->title;
+$this->params['breadcrumbs'][] = ['label' => 'Посты', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Изменение';
 ?>
 <div class="post-update">
 
