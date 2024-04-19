5feba481f6f45f05ed75f2d21e731458be920922
ivanov.a.a.23@mail.ru <rainboowbee@gmail.com>
2024-04-19 12:45:02 +0300

    Настроил миграцию для создания и заполнения таблицы category

---
+++ console/migrations/m240419_092826_create_category_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%category}}`.
+ */
+class m240419_092826_create_category_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('category', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+        ]);
+
+        Yii::$app->db->createCommand()->batchInsert('category', ['title'], [
+            ['Food'],
+            ['LifeStyle'],
+            ['Photography'],
+        ])->execute();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('category');
+    }
+}
